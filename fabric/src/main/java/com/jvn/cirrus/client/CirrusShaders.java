da7946f85b993205cb526210a603684ed9bdea61
jvn <73175916+JevenDev@users.noreply.github.com>
2026-08-23 12:51:32 -0400

    Fix Milky Way lookup texture preload

--- fabric/src/main/java/com/jvn/cirrus/client/CirrusShaders.java
+++ fabric/src/main/java/com/jvn/cirrus/client/CirrusShaders.java
@@ -147,6 +147,7 @@ public final class CirrusShaders {
         minecraft.getTextureManager().getTexture(
                 Identifier.withDefaultNamespace("textures/environment/clouds.png")
         );
+        minecraft.getTextureManager().getTexture(Cirrus.texture("environment/milky_way_lookup.png"));
         minecraft.getTextureManager().getTexture(Cirrus.texture("environment/north_star.png"));
         samplerTexturesLoaded = true;
     }
