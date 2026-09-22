7ef6921141de07e1c9ebda91bdb39cfa6eb120bf
jvn <73175916+JevenDev@users.noreply.github.com>
2026-09-22 12:10:22 -0400

    Preserve cloud depth for post effects with DH

--- fabric/src/main/java/com/jvn/cirrus/mixin/LevelRendererCloudMixin.java
+++ fabric/src/main/java/com/jvn/cirrus/mixin/LevelRendererCloudMixin.java
@@ -136,7 +136,8 @@ public abstract class LevelRendererCloudMixin {
                     ticks,
                     CirrusRenderContext.camera().position().x,
                     CirrusRenderContext.camera().position().y,
-                    CirrusRenderContext.camera().position().z
+                    CirrusRenderContext.camera().position().z,
+                    false
             );
         } finally {
             CirrusRenderContext.endRenderingCloudsForDistantHorizons();
